ecb4f4363248bab1ecb045684699fb74b7c7d229
Lluis Casamajor <lluischess@gmail.com>
2021-04-26 22:47:37 +0200

    Webapp blog

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Blog.webapp.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('webapp.urls')),
+    path('', home, name='home'),
 ]
--- Blog/webapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from Blog.webapp.views import home
-
-urlpatterns = [
-    path('', home, name="home"),
-]
