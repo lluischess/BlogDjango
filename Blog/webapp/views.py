3c55007f96cac394708f7641fc69b613124cf976
Lluis Casamajor <lluischess@gmail.com>
2021-04-27 21:09:49 +0200

    Content

--- Blog/webapp/views.py
+++ Blog/webapp/views.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
-def home():
-    return render(request='home.html')
+def home(request):
+    return render(request, 'home.html')
